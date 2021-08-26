437652ea1344c7bab25930c4915a5ae34d12d4bb
mayukorin <mayukuma@r.sea.sannet.ne.jp>
2021-08-26 16:13:37 +0900

    Implement advanced login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -37,4 +37,18 @@ module SessionsHelper
         @current_user = nil
     end
 
+    # 渡されたユーザーがカレントユーザーであればtrueを返す
+    def current_user?(user)
+        user && user == current_user
+    end
+
+    def redirect_back_or(default)
+        redirect_to(session[:forwarding_url] || default)
+        session.delete(:forwarding_url)
+    end
+
+    def store_location
+        session[:forwarding_url] = request.original_url if request.get?
+    end
+
 end
